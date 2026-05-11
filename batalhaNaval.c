8e5e273fe15c2e5dbd2da3f23f5abb3cc809b52e
Francisco Ednando <ednando.co@hotmail.com>
2026-05-11 22:32:57 +0000

    Acrescentei as diagonais principal e secundária. Desafio Aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -17,6 +17,25 @@ int main() {
     for(int i = 5; i<=7; i++) {
         tabuleiro[i][4] = 3;
     }
+
+    //Navio na diagonal principal (aqui a linha aumenta e a coluna também aumenta)
+    int diagonal1_linha = 0, diagonal1_coluna = 6; //marca a posição inicial
+    for(int i = 0; i <3; i++) {
+        int l = diagonal1_linha + i; //Calcula a linha atual
+        int c = diagonal1_coluna + i; //Calcula a coluna atual
+        if (l < 10 && c < 10 && tabuleiro[l][c] == 0) { //Limite do tabuleiro
+            tabuleiro[l][c] = 3; //Coloca o navio na posição
+        }
+    }
+    //Navio na diagonal secundária (linha aumenta, coluna vai diminuir)
+    int diagonal2_linha = 5, diagonal2_coluna = 9; //marca a posição inicial
+    for(int i = 0; i <3; i++) {
+        int l = diagonal2_linha + i; //Calcula a linha atual
+        int c = diagonal2_coluna - i; //Calcula a coluna atual
+        if (l < 10 && c >= 0 && tabuleiro[l][c] == 0) { //Limite do tabuleiro
+            tabuleiro[l][c] = 3; //Coloca o navio na posição
+        }
+    }
     printf(" Tabuleiro Batalha Naval\n");
     printf ("  ");
     for (int j = 0; j <10; j++) {
